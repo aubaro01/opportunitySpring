cd639954b5d1a2916a68b1a5617e91f89d24b73d
Cláudio Barroso <109551603+aubaro01@users.noreply.github.com>
2026-07-13 10:10:34 +0100

    Create opportunity adapter and core components

---
+++ src/main/java/aubaro/opportunity/adapter/dto/request/createOpportunity.java
@@ -0,0 +1,15 @@
+package aubaro.opportunity.adapter.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class createOpportunity {
+
+    private String name;
+}
